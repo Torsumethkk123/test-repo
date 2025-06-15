df324aabe942041641db19379727e365633de421
Torsumethkk123 <torsumethkkshin@gmail.com>
2025-06-15 12:40:50 +0700

    Add cat function in main.go

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
+type Speakable interface {
+	Speak()
+}
+
+type Cat struct {
+	Name string
+}
+
+func (c Cat) Speak() {
+	fmt.Println(c.Name + ": Meow")
+}
+
 func main() {
+	var cat Speakable
+	cat = Cat{ Name: "Gigy" }
 	fmt.Println("Hello, <Main.go>")
+	cat.Speak()
 }
